dfd2701837becc1d4d28fda27e9fa365c3c0bec8
Knowgood93 <96952424+Knowgood93@users.noreply.github.com>
2024-09-14 08:47:48 -0600

    Add files via upload

---
+++ en.lua
@@ -0,0 +1,37 @@
+Locales = Locales or {}
+Locales['en'] = {
+    ['buy_waypoint_plug'] = 'Buy Waypoint to Plug ($%s)',
+    ['buy_waypoint_gun'] = 'Buy Waypoint to Gun Dealer ($%s)',
+    ['you_have_reached_waypoint'] = 'You have reached the waypoint! You have limited time to make your purchases.',
+    ['not_enough_money'] = 'You do not have enough money!',
+    ['one_waypoint_per_session'] = 'You can only purchase one waypoint per session!',
+    ['time_up'] = 'Time\'s up! You need to buy the waypoint again.',
+    ['plug_in_jail'] = 'The Plug has been sent to jail!',
+    ['gun_in_jail'] = 'The Gun Dealer has been sent to jail!',
+    ['bail_success'] = 'You have bailed out the NPC!',
+    ['bail_fail'] = 'You do not have enough money to bail out the NPC!',
+    ['purchase_success'] = 'Success: You have purchased %s!',
+    ['purchase_fail_money'] = 'Error: You do not have enough money to purchase %s!',
+    ['inventory_full'] = 'Error: Your inventory is full! Cannot add %s.',
+    ['inventory_fail'] = 'Error: Failed to add item to inventory! Please try again.',
+    ['suspicious_activity'] = '10-15 - Suspicious Activity',
+    ['suspicious_message'] = 'A suspicious individual is conducting illegal activities at %s',
+    ['plug_label'] = 'Plug',
+    ['plug_label_jail'] = 'Plug (In Jail)',
+    ['gun_label'] = 'Gun Dealer',
+    ['gun_label_jail'] = 'Gun Dealer (In Jail)',
+    ['bail_cop_label'] = 'Bail Cop',
+    ['pay_bail'] = 'Pay Bail ($%s)',
+
+    -- Plug NPC Items
+    ['buy_kilo_coke'] = 'Buy Kilo of Coke ($%s)',
+    ['buy_kilo_weed'] = 'Buy Kilo of Weed ($%s)',
+    ['buy_kilo_heroin'] = 'Buy Kilo of Heroin ($%s)',
+    ['buy_kilo_meth'] = 'Buy Kilo of Meth ($%s)',
+
+    -- Gun NPC Items
+    ['buy_pistol'] = 'Buy Pistol ($%s)',
+    ['buy_smg'] = 'Buy SMG ($%s)',
+    ['buy_shotgun'] = 'Buy Shotgun ($%s)',
+    ['buy_rifle'] = 'Buy Assault Rifle ($%s)',
+}
